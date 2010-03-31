3dc53c2eb43cd5e93e0e2bf2ae310dda98f1a933
Craig Smith <craigmarksmith@Craig-Smiths-MacBook-Pro.local>
2010-03-31 19:27:35 +0100

    Make navigation of movies easier my showing 1 movie per page

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,10 @@
 class MoviesController < ApplicationController
 
+  def show
+    @movies = Movie.all(:order => 'sequence_number ASC')
+    @movie = params[:id] ? Movie.find(params[:id]) : @movies.first
+  end
+
   def index
     @movies = Movie.all(:order => 'sequence_number ASC')
   end
--- config/routes.rb
+++ config/routes.rb
@@ -32,7 +32,7 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  map.root :controller => "movies"
+  map.root :controller => "movies", :action => "show"
 
   map.login 'login', :controller => 'user_sessions', :action => 'new'
   map.logout 'logout', :controller => 'user_sessions', :action => 'destroy'
