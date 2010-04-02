26f0a4c52baae7d58d9e41524dc71a12be965a59
Craig Smith <craigmarksmith@Craig-Smiths-MacBook-Pro.local>
2010-04-02 09:15:42 +0100

    permalinks must be unique

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,7 @@
 class Movie < ActiveRecord::Base
 
-  before_save :set_permalink
+  before_validation :set_permalink
+  validates_uniqueness_of :permalink
 
   def set_permalink
     self.permalink = title.downcase.gsub(/'/,'').gsub(/[^a-z0-9]/,'-').gsub(/-+/,'-')
