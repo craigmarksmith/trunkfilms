a4cf15537627bf1d74005c00bb4226ff168b1ba8
Craig Smith <craigmarksmith@Craig-Smiths-MacBook-Pro.local>
2010-04-03 15:49:20 +0100

    Won't run

--- db/migrate/20100402074247_add_permalink_to_movie.rb
+++ db/migrate/20100402074247_add_permalink_to_movie.rb
@@ -2,8 +2,6 @@ require 'movie'
 class AddPermalinkToMovie < ActiveRecord::Migration
   def self.up
     add_column :movies, :permalink, :string
-    
-    Movie.all.each {|m| m.set_permalink.save}
   end
 
   def self.down
