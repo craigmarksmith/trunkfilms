ec106036afcc075f0b4402c01e9f743e367251b4
Craig Smith <craigmarksmith@unknown-00-26-bb-10-a7-a8.home>
2010-05-06 20:48:52 +0100

    Order the blog posts by date.

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,11 +1,11 @@
 class BlogController < ApplicationController
 
   def index
-    @posts = BlogPost.all
+    @posts = BlogPost.all(:order => 'created_at DESC')
   end
 
   def show
-    @posts = BlogPost.all
+    @posts = BlogPost.all(:order => 'created_at DESC')
     @post = BlogPost.find_by_permalink(params[:id])
   end
 end
